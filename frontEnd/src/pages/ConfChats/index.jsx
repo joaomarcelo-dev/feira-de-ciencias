87db1425ca3ce7d523cab4bd18f45c06ed5fa16f
joaomarcelo-dev <joaomarcelolimaoliveira7@gmail.com>
2023-12-05 09:53:07 -0300

    pos apresentação

--- frontEnd/src/pages/ConfChats/index.jsx
+++ frontEnd/src/pages/ConfChats/index.jsx
@@ -1,6 +1,9 @@
+import Header from "../../components/Header";
+
 function ConfChats() {
   return (
     <div>
+      <Header />
       <h1>ConfChats</h1>
     </div>
   )
--- frontEnd/src/pages/Home/index.jsx
+++ frontEnd/src/pages/Home/index.jsx
@@ -6,8 +6,12 @@ import Footer from "../../components/Footer";
 import socket from "../../services/socket";
 import NoMessage from "../NoMessage";
 
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+import { useState } from "react";
+
 function Home() {
   const { message: { message, messageNoCryp, user: { name } } } = useSelector((state) => state.app);
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
   const handleResetApp = () => {
     socket.emit('reset-app');
@@ -45,8 +49,25 @@ function Home() {
               <div className="container-message-descryp container-message">
                 <h2 className="title-message">Mensagem não criptografada:</h2>
 
-                <div className="content-message">
-                  <h3 className="text-message">{ messageNoCryp }</h3>
+                <div className="flex-row message-no-cripty">
+                  <div className="content-message">
+                    <h3
+                      className={`text-message ${ passwordVisible ? '' : 'text-invisible' }`}
+                    >
+                      { messageNoCryp }
+                    </h3>
+                  </div>
+                  
+                  <button
+                    className="button-visible-password"
+                    onClick={() => setPasswordVisible(!passwordVisible)}
+                  >
+                    {
+                      passwordVisible
+                      ? <AiOutlineEye size={50} onClick={() => setPasswordVisible(true)} />
+                      : <AiOutlineEyeInvisible size={50} onClick={() => setPasswordVisible(false)} />
+                    }
+                  </button>
                 </div>
               </div>
             </div>
